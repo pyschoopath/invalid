8a2a00b98cc75cb094a86fe1583e56fdb94f9927
pyschoopath <47376855+pyschoopath@users.noreply.github.com>
2019-02-06 01:44:40 -0800

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Count 
+{
+
+    public static void main(String[] args) 
+    {
+        String line = "This website is aw3som3.";
+        int vowels = 0, consonants = 0;
+
+        line = line.toLowerCase();
+        for(int i = 0; i < line.length(); ++i)
+        {
+            char ch = line.charAt(i);
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') 
+                {
+                ++vowels;
+            }
+            else if((ch >= 'a'&& ch <= 'z')) 
+            {
+                ++consonants;
+            }
+            
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        
+    }
+}
